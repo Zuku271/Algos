593f37ba050c6eba353492daba23f211f2632e13
Zuku271 <adrian.zuchowski@outlook.com>
2017-10-07 17:21:55 +0200

    Tests of second version.

--- C++/source/Converting/IntToBin.cpp
+++ C++/source/Converting/IntToBin.cpp
@@ -2,6 +2,7 @@
 Integer to Binary conversion algorithm
 */
 #include <vector>
+#include <iostream>
 
 void IntToBin(int _integer, std::vector<char> &bin)
 {
@@ -9,4 +10,15 @@ void IntToBin(int _integer, std::vector<char> &bin)
         return;
     bin.push_back(_integer%2);
     IntToBin(_integer/2, bin);
+}
+
+int main()
+{
+    std::vector<char> temp;
+    int a = 10;
+    IntToBin(a, temp);
+
+    std::cout << temp[0]+0x30 << '\n';
+
+    return 0;
 }
